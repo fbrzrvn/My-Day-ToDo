cc7f62cf5a5a7d546d569001d027914151ac4b7a
fabrizio <faber.rvn@gmail.com>
2021-01-04 14:41:37 +0100

    #6 Add to-do

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,10 @@ import { getDate } from './model/date.js';
 import { toggleTaskEvent } from './views/taskForm.js';
 import { toggleListEvent } from './views/listForm.js';
 import { handleRenderTaskList } from './views/renderTaskList.js';
+import { createToDo, getTask } from './controller/task.js';
+
+
+const createTaskBtn = document.getElementById('create-task-btn');
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,8 +13,9 @@ window.addEventListener('DOMContentLoaded', () => {
   toggleTaskEvent();
   toggleListEvent();
   handleRenderTaskList();
+  getTask();
 })
-
+createTaskBtn.addEventListener('click', createToDo);
 
 
 
